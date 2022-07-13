51d3814c6d38c6b7612bc8c9ef35975a87d11c0c
Gino Rey <gino.rey@Outlook.com>
2022-07-13 14:48:08 -0400

    commit to main

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
-tmux kill-server
-echo "There are currently no servers running! Congratz :)"
 cd ~/pe-portfolio-site
 git fetch && git reset origin/main --hard
 source python3-virtualenv/bin/activate
 pip install --upgrade pip
 pip3 install -r requirements.txt
-tmux new-session -d -s pe-portoflio-site 'source python3-virtualenv/bin/activate && flask run --host=0.0.0.0'
+systemctl daemon-reload
+systemctl restart myportfolio
